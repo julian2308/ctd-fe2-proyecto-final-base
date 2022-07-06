23e6d0f334ff1f41a519fb99ba95c33890ce74e7
julian2308 <julian.d.alvarado23@gmail.com>
2022-07-05 23:30:46 -0500

    Most of the test are done!

--- src/features/quote/Quote.test.tsx
+++ src/features/quote/Quote.test.tsx
@@ -1,19 +1,65 @@
-import { fireEvent, screen } from "@testing-library/react";
-import { render } from "../../test-utils"
+import { fireEvent, screen, waitFor } from "@testing-library/react";
+import { render } from "../../test-utils";
+import { setupServer } from "msw/node";
 import Cita from "./Cita";
+import { quoteWithoutName, handlers } from "../../mocks/handler";
 
-it("bababa", () => {
+const server = setupServer(...handlers);
 
-    render(<Cita/>)
-    const textoInicial = screen.queryByText("No se encontro ninguna cita")
-    expect(textoInicial).toBeInTheDocument()
+beforeAll(() => server.listen());
+afterAll(() => server.close);
+afterEach(() => server.resetHandlers);
 
-})
+describe("First render of the application", () => {
+  it("Should render the main text", () => {
+    render(<Cita />);
+    const initialText = screen.queryByText("No se encontro ninguna cita");
+    expect(initialText).toBeInTheDocument();
+  });
 
-it("bababa", () => {
+  it("Should render the search input by name", () => {
+    render(<Cita />);
+    const inputName = screen.queryByPlaceholderText(
+      "Ingresa el nombre del autor"
+    );
+    expect(inputName).toBeInTheDocument();
+  });
 
-    render(<Cita/>)
-    const inputNombre = screen.queryByPlaceholderText("Ingresa el nombre del autor")
-    expect(inputNombre).toBeInTheDocument()
+  it("Should render the button of search", () => {
+    render(<Cita />);
+    const getQuoteBtn = screen.queryByText("Obtener cita aleatoria", {
+      selector: "button",
+    });
+    expect(getQuoteBtn).toBeInTheDocument();
+  });
 
-})
+  it("Should render the clear button", () => {
+    render(<Cita />);
+    const clearBtn = screen.queryByText("Borrar", { selector: "button" });
+    expect(clearBtn).toBeInTheDocument();
+  });
+});
+
+describe("Using the application with different arguments", () => {
+  it("Should display a 'random' quote", async () => {
+    render(<Cita />);
+    const getQuoteBtn = screen.getByText("Obtener cita aleatoria", {
+      selector: "button",
+    });
+    fireEvent.click(getQuoteBtn);
+    await waitFor(() =>
+      expect(screen.queryByText(quoteWithoutName.quote)).toBeInTheDocument()
+    );
+  });
+
+  it("Should display a 'random' name", async () => {
+    render(<Cita />);
+    const getQuoteBtn = screen.getByText("Obtener cita aleatoria", {
+      selector: "button",
+    });
+    fireEvent.click(getQuoteBtn);
+    await waitFor(() =>
+      expect(screen.queryByText(quoteWithoutName.character)).toBeInTheDocument()
+    );
+  });
+});
--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,13 +1,23 @@
-import {rest} from 'msw'
-import { API_URL } from '../app/constants'
+import { rest } from "msw";
+import { API_URL } from "../app/constants";
 
-export const handlers = [
+const quoteWithoutName = {
+  quote: "Oh, so they have Internet on computers now!",
+  character: "Homer Simpson",
+  image:
+    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+  characterDirection: "Right",
+}
 
-    rest.get(API_URL, (req, res, ctx) => {
+const handlers = [
+  rest.get(API_URL, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        quoteWithoutName,
+      ])
+    );
+  }),
+];
 
-        return res(
-            ctx.status(200)
-        )
-    })
-
-]
+export {quoteWithoutName, handlers}
